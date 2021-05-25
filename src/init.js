e5ba2a862a42c6232dda68fd50711adb9e30ef29
strelov1 <strelov1@gmail.com>
2021-05-25 09:05:37 +0300

    add translations

--- src/init.js
+++ src/init.js
@@ -1,3 +1,6 @@
+import i18next from 'i18next';
+import { setLocale } from 'yup';
+
 import watchers from './watchers';
 import bindControllers from './controllers';
 import render from './view';
@@ -17,8 +20,47 @@ export default () => {
 
   const container = document.getElementById('root');
 
-  watchers(initState, (state) => {
-    render(container, state);
-    bindControllers(container, state);
+  i18next.init({
+    lng: 'ru',
+    resources: {
+      en: {
+        translation: {
+          example: 'Example',
+          feeds: 'Feeds',
+          posts: 'Posts',
+          forms: {
+            success: '',
+            validation: {
+              invalid: '',
+              required: '',
+            },
+          },
+        },
+      },
+      ru: {
+        translation: {
+          example: 'Пример',
+          feeds: 'Фиды',
+          posts: 'Посты',
+          forms: {
+            validation: {
+              invalid: '',
+              required: '',
+            },
+          },
+        },
+      },
+    },
+  }).then(() => {
+    setLocale({
+      mixed: {
+        default: i18next.t('forms.validation.invalid'),
+        required: i18next.t('forms.validation.required'),
+      },
+    });
+    watchers(initState, (state) => {
+      render(container, state, i18next);
+      bindControllers(container, state);
+    });
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,6 +1,9 @@
 import template from './templates/main.mustache';
 
-export default (container, state) => {
+export default (container, state, i18next) => {
   // eslint-disable-next-line no-param-reassign
-  container.innerHTML = template(state);
+  container.innerHTML = template({
+    ...state,
+    i18n: () => (key) => i18next.t(key),
+  });
 };
