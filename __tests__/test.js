7a181ed188356daabf1d89d08ca25c317f945d97
strelov1 <strelov1@gmail.com>
2021-05-29 15:08:20 +0300

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,15 +1,11 @@
-// @ts-check
 import { beforeEach, test, expect } from '@jest/globals';
 
 import { promises as fs } from 'fs';
 import path from 'path';
-// import { fileURLToPath } from 'url';
 import init from '../src/init';
 
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 beforeEach(async () => {
-  const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
+  const pathToHtml = path.resolve(__dirname, '../', 'index.html');
   const html = await fs.readFile(pathToHtml, 'utf8');
   document.body.innerHTML = html;
 });
